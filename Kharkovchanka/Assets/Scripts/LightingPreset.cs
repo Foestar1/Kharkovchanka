7ec2dd9648e8ef4dad82f631bde129f2866a36be
Foestar1 <118066929+Foestar1@users.noreply.github.com>
2023-12-23 22:54:01 -0500

    New Day/Night Cycle
    Testing new cycle system

--- Kharkovchanka/Assets/Scripts/LightingPreset.cs
+++ Kharkovchanka/Assets/Scripts/LightingPreset.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class LightingPreset : MonoBehaviour
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptable/Lighting Preset", order = 1)]
+public class LightingPreset : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
 }
